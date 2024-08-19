ea44733ffca4ee359f3e7899962ab20e92803176
sangmin <j__sm@naver.com>
2024-08-19 15:18:50 +0900

    스프링 테스트 코드

--- teamcostco/src/main/java/com/ezentwix/teamcostco/TeamcostcoApplication.java
+++ teamcostco/src/main/java/com/ezentwix/teamcostco/TeamcostcoApplication.java
@@ -2,6 +2,8 @@ package com.ezentwix.teamcostco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TeamcostcoApplication {
@@ -10,4 +12,11 @@ public class TeamcostcoApplication {
 		SpringApplication.run(TeamcostcoApplication.class, args);
 	}
 
+	@RestController
+	class HelloWorld {
+		@GetMapping("/")
+		public String getMethodName() {
+			return "Hello";
+		}
+	}
 }
