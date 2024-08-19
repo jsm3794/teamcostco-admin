7984e486533109f93421d889d30f0a87a1f569c7
sangmin <j__sm@naver.com>
2024-08-19 14:22:17 +0900

    Maven 프로젝트 생성

---
+++ teamcostco/src/main/java/com/ezentwix/teamcostco/TeamcostcoApplication.java
@@ -0,0 +1,13 @@
+package com.ezentwix.teamcostco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcostcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcostcoApplication.class, args);
+	}
+
+}
---
+++ teamcostco/src/test/java/com/ezentwix/teamcostco/TeamcostcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezentwix.teamcostco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcostcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
