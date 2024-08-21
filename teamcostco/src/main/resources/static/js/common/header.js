55345ec49e84d10aeb134756aaf5fb5d283eca55
admin <admin@naver.com>
2024-08-21 16:02:29 +0900

    네비 색 변경, 필요없는 아이콘 삭제, 메인 컨테이너에 검색창 추가

--- teamcostco/src/main/resources/static/js/common/header.js
+++ teamcostco/src/main/resources/static/js/common/header.js
@@ -24,18 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// 알림 아이콘 노드 가져오기 (처음에 hidden)
-const bell = document.getElementById("bell");
-bell.style.visibility = 'hidden';  // 초기 상태에서 숨김 처리
-
 // 알림 아이콘 클릭 이벤트
 document.getElementById('notification-icon').addEventListener('click', function () {
-    // 아이콘을 클릭할 때 visibility를 visible로 변경
-    bell.style.visibility = 'visible';  // bell 노드가 보이도록 설정
-    this.innerHTML = '';  // 기존 알림 아이콘을 비우고
-    this.appendChild(bell);  // bell 아이콘을 notification-icon 안에 삽입
+    // 알림 아이콘을 클릭하면 알림 내용 제거
+    const notificationContainer = this.querySelector('.notifications');
+    while (notificationContainer.firstChild) {
+        notificationContainer.removeChild(notificationContainer.firstChild);
+    }
+    const notificationIcon = document.querySelector('.material-symbols-outlined');
+    notificationIcon.textContent = 'notifications';
 });
-
 // 프로필 이미지 클릭 시 페이지 이동
 document.querySelector('.profile-pic').addEventListener('click', function () {
     window.location.href = '/mypage';
