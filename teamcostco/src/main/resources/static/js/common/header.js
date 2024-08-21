7b216a0960726a111c707c357d96ea57ccfb0d4d
uiwoowP <uiwoowP@gmail.com>
2024-08-21 10:34:30 +0900

    header와 nav 기능 추가

--- teamcostco/src/main/resources/static/js/common/header.js
+++ teamcostco/src/main/resources/static/js/common/header.js
@@ -1,2 +1,32 @@
 /* header script를 기술할 js 파일입니다. */
 
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdown = document.querySelector('.shop-dropdown');
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    let timeout;
+    dropdownBtn.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+        dropdownContent.style.display = 'block';
+    });
+    dropdownBtn.addEventListener('mouseleave', () => {
+        timeout = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+        }, 200);
+    });
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+    });
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeout = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+        }, 200);
+    });
+});
+const bell = document.getElementById("bell").outerHTML;
+document.getElementById('notification-icon').addEventListener('click', function () {
+    this.innerHTML = bell;
+});
+document.querySelector('.profile-pic').addEventListener('click', function () {
+    window.location.href = '/mypage';
+});
--- teamcostco/src/main/resources/static/js/common/navigationbar.js
+++ teamcostco/src/main/resources/static/js/common/navigationbar.js
@@ -2,15 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const navItems = document.querySelectorAll('.nav-item');
-
+    const headerTitle = document.getElementById('header-title');
+    const current_page = document.getElementById("current-page");
     navItems.forEach(item => {
         item.addEventListener('click', function() {
-
             navItems.forEach(navItem => navItem.classList.remove('active'));
-
             this.classList.add('active');
-
+            let target = this.getAttribute('data-target');
+            if (target.startsWith('#')) {
+                target = target.slice(1);
+            }
+            target = target.charAt(0).toUpperCase() + target.slice(1);
+            console.log('Navigating to:', target);
             console.log('Navigating to:', this.getAttribute('data-target'));
+            headerTitle.textContent = `${target}`;
+            current_page.innerText = this.getAttribute('data-target');
         });
     });
 });
