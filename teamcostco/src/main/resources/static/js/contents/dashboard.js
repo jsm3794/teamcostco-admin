74fb223943d9c7507ce161f62927b0b726f8274d
admin <admin@naver.com>
2024-08-28 16:58:40 +0900

    total product  통계박스 쿼리문 수정, 대시보드 박스 비율 수정

--- teamcostco/src/main/resources/static/js/contents/dashboard.js
+++ teamcostco/src/main/resources/static/js/contents/dashboard.js
@@ -40,30 +40,34 @@ const chart2 = document.getElementById('chart2').getContext('2d');
 const myChart2 = new Chart(chart2, {
     type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+        responsive: false,
+        plugins: {
+            legend: {
+                position: 'right'  // 라벨을 오른쪽에 배치
+            }
+        },
         scales: {
             y: {
                 beginAtZero: true
@@ -79,28 +83,84 @@ const chart3 = document.getElementById('chart3').getContext('2d');
 const mychart3 = new Chart(chart3, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 7, 5, 9],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
             ],
             borderWidth: 1
-        }]
+        },
+        {
+            label: '# of Votes',
+            data: [3, 21, 2, -3, 1],
+            backgroundColor: [
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '# of Votes',
+            data: [19, 14, 3, 1, 2],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, -1],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1
+        }
+        ]
     },
     options: {
         scales: {
@@ -118,31 +178,29 @@ const chart4 = document.getElementById('chart4').getContext('2d');
 const myChart4 = new Chart(chart4, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
-        indexAxis:'y',
+        indexAxis: 'y',
         scales: {
             x: {
                 beginAtZero: true
@@ -150,3 +208,33 @@ const myChart4 = new Chart(chart4, {
         }
     }
 });
+
+
+// 3박스 관련 함수
+function initializeInventory() {
+    fetchProductSummary();
+    // 기타 인벤토리 페이지 초기화 로직
+}
+
+function fetchProductSummary() {
+    fetch('/productsummary')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("categoryCount").textContent = data.totalCategories;
+            document.getElementById("totalQtySum").textContent = data.totalProductsQty;
+            document.getElementById("lowStockCount").textContent = data.lowProducts;
+        })
+        .catch(error => console.error('Error fetching product summary:', error));
+}
+
+// 10분마다 데이터 갱신
+setInterval(fetchProductSummary, 600000);
+
+// 개수 표시 div 클릭시 상세창
+$(".info-box").click((e) => {
+    console.log("상세보기창 만들수도");
+});
+// 개수 최초 데이터 로드
+fetchProductSummary();
+// 10분마다 개수 데이터 갱신 (밀리초 단위)
+setInterval(fetchProductSummary, 600000);
