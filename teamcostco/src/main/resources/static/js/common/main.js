55e5b26d180f109c42f561f0d99c53eb0ea9de17
sangmin <j__sm@naver.com>
2024-09-02 18:24:28 +0900

    테이블 열 조정 애니메이션 추가

--- teamcostco/src/main/resources/static/js/common/main.js
+++ teamcostco/src/main/resources/static/js/common/main.js
@@ -1,42 +1,3 @@
-function setTableColumnWidths(table, widths) {
-    if (!table || !widths || !Array.isArray(widths)) {
-        throw new Error('Invalid arguments');
-    }
-
-    // Ensure that the table has a thead and tbody
-    const thead = table.querySelector('thead');
-    const tbody = table.querySelector('tbody');
-
-    if (!thead || !tbody) {
-        throw new Error('Table must have both thead and tbody');
-    }
-
-    // Apply widths to thead th elements
-    const ths = thead.querySelectorAll('th');
-    for (let i = 0; i < widths.length; i++) {
-        ths[i].style.width = widths[i];
-    }
-
-    // Set padding for the last th element
-    if (ths.length > 0) {
-        const scrollbarWidth = thead.clientWidth - tbody.clientWidth;
-        ths[ths.length - 1].style.setProperty('padding-right', `${scrollbarWidth}px`, 'important');
-        ths[ths.length - 1].style.setProperty('width', '0px', 'important');
-    }
-
-    // Apply widths to tbody td elements
-    const rows = tbody.querySelectorAll('tr');
-    rows.forEach(row => {
-        const tds = row.querySelectorAll('td');
-        tds.forEach((td, index) => {
-            if (widths[index]) {
-                td.style.width = widths[index];
-            }
-        });
-    });
-}
-
-
 const table = document.querySelector('#results');
 const thead = table.querySelector('thead');
 const tbody = table.querySelector('tbody');
@@ -44,7 +5,6 @@ const thElements = thead.querySelectorAll('th');
 const tdElements = tbody.querySelectorAll('tr:first-child td');
 
 function applyWidths() {
-
     if (thElements.length - 1 !== tdElements.length) {
         return;
     }
@@ -60,6 +20,8 @@ function applyWidths() {
             });
         }
     });
+
+    tbody.classList.add('show');
 }
 
 // MutationObserver 설정
